052fe0c4a21e845af033e1ed86e27f65bc81207e
Jiakuan Li <jiakuan.li.cs@gmail.com>
2017-07-20 16:38:11 -0400

    download qemu

---
+++ qemu.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/qemu/qemu.git
+cd qemu
+git checkout v2.1.3
+patch -p1 < ../myqemu.diff
+
+./configure --target-list=x86_64-linux-user --enable-debug
+make -j4
+
+export QEMU=x86_64-linux-user
