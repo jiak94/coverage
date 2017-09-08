5ac81d2fd7371eb12efeedce62f6ed6da8f406a2
Jiakuan Li <jiakuan.li.cs@gmail.com>
2017-09-08 18:06:54 -0400

    code clean up

--- bb_lift.py
+++ bb_lift.py
@@ -6,35 +6,16 @@ def analysis():
     all_funcs = idautils.Functions()
     overall_addr = list()
 
-    # addr_overall = open('addr_overall', 'a+')
-    # addr_bin = open('addr_bin', 'a+')
-
-    # addr_lib = open('addr_lib', 'a+')
     print all_funcs
     for f in all_funcs:
         print f, hex(f)
         print "In %s:\n"%(idc.GetFunctionName(f),)
 
-
-        # fflags = idc.GetFunctionFlags(f)
-        # print type(fflags)
-        # print fflags
-
         f = idaapi.FlowChart(idaapi.get_func(f),flags=idaapi.FC_PREDS)
 
         for block in f:
-            # addr_overall.write(hex(block.startEA))
-            # addr_overall.write('\n')
             overall_addr.append(hex(block.startEA))
 
-            # if fflags == FUNC_LIB:
-            #     # addr_lib.write(hex(block.startEA))
-            #     # addr_lib.write('\n')
-            #     lib_addr.append(hex(block.startEA))
-            # else:
-            #     # addr_bin.write(hex(block.startEA))
-            #     # addr_bin.write('\n')
-            #     bin_addr.append(hex(block.startEA))
     filename = idc.GetInputFile() + "_addr"
     with open(filename, 'a+') as f:
         for item in overall_addr:
--- coverage.py
+++ coverage.py
@@ -2,11 +2,9 @@ import shlex, psutil, subprocess, sys, os, time
 from datetime import datetime
 import json
 
-global global_hit, so_global_hit
-global_hit = list()
-so_global_hit = dict()
-# global start_code, end_code
-# global program
+global binary_overall_hit, so_overall_hit
+binary_overall_hit = list()
+so_overall_hit = dict()
 
 def capture_bb(filename):
     with open(filename, 'r') as bb_file:
@@ -16,22 +14,23 @@ def capture_bb(filename):
     return bb_list
 
 def calculate_lib_addr(maps):
-    res = dict()
-    for lib in lib_list:
-        base_addr = maps[lib][0]
-        base_addr = int(base_addr, 16)
+    pass
+#     res = dict()
+#     for lib in lib_list:
+#         base_addr = maps[lib][0]
+#         base_addr = int(base_addr, 16)
 
-        lib_addr_file = lib+"_addr"
-        with open(lib_addr_file, 'r') as lib_bb:
-            lib_map = lib_bb.readlines()
-        lib_map = [x.strip() for x in lib_map]
+#         lib_addr_file = lib+"_addr"
+#         with open(lib_addr_file, 'r') as lib_bb:
+#             lib_map = lib_bb.readlines()
+#         lib_map = [x.strip() for x in lib_map]
 
-        for idx, item in enumerate(lib_map):
-            new_addr = hex(base_addr + int(item, 16))
-            lib_map[idx] = new_addr
+#         for idx, item in enumerate(lib_map):
+#             new_addr = hex(base_addr + int(item, 16))
+#             lib_map[idx] = new_addr
 
-        res[lib] = lib_map
-    return res
+#         res[lib] = lib_map
+#     return res
 
 def calculate_lib_offset(maps, lib_addr):
     res = dict()
@@ -40,8 +39,7 @@ def calculate_lib_offset(maps, lib_addr):
             base_addr = maps[lib][0]
             base_addr = int(base_addr, 16)
             res[lib] = list()
-            # for index, val in enumerate(lib_addr[lib]):
-            #     res[lib][index] = hex(int(lib_addr[lib][index], 16) - base_addr)
+
             for addr in lib_addr[lib]:
                 res[lib].append(hex(int(addr, 16) - base_addr))
 
@@ -51,29 +49,32 @@ def calculate_lib_offset(maps, lib_addr):
 
     return res
 
-def capture_log(program_exec_command, logname, testcase, lib_list, main_addr, statistic):
-    command = usermode_command + " " + logname + " " + "-b " + str(main_addr) + " " + program_exec_command
+def capture_log(program_exec_command, logname, testcase, lib_list):
+    command = usermode_command + " " + logname + " " + "-b " + " ".join(program_exec_command)
 
-    proc = subprocess.Popen(shlex.split(command), shell=False, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    # pause qemu
+    proc = subprocess.Popen(shlex.split(command),
+                            shell=False,
+                            stderr=subprocess.PIPE,
+                            stdout=subprocess.PIPE)
 
     p = psutil.Process(proc.pid)
 
-    # import IPython; IPython.embed()
     while (p.status() is 'running'):
         time.sleep(1)
 
-    mem_map = capture_maps(proc.pid)
+    mem_map = get_mem_mapping(proc.pid)
 
-    if statistic:
-        mem_bb = calculate_lib_addr(mem_map)
+    # if statistic:
+        # calculate the REAL addr for target library
+        # libs_loaded_addr = calculate_lib_addr(mem_map)
+
     # try to resume process
     p.resume()
 
     # resume fail, kill that process, then execute again without pause
     if p.status is not 'running':
         p.kill()
-        command = usermode_command + " " + logname + " " + program_exec_command
+        command = usermode_command + " " + logname + " " + " ".join(program_exec_command)
         proc = subprocess.Popen(shlex.split(command),
                                 shell=False,
                                 stderr=subprocess.PIPE,
@@ -82,63 +83,63 @@ def capture_log(program_exec_command, logname, testcase, lib_list, main_addr, st
 
     stderr = proc.stderr.read()
 
-    # write log if any testcase crash or failed
+    # write log if any std err return
     if len(stderr) is not 0:
-        print "Error occurs while executing " + testcase
+        print "Error occurs while executing " + str(testcase)
         print "Program or Qemu crashed, please check error log for more information"
         write_error_log(testcase, stderr)
 
-    binary_bb_hit, mem_bb_hit = process_trace(logname, mem_map)
-    if statistic:
-        return binary_bb_hit, mem_bb_hit, mem_bb
-    else:
-        return binary_bb_hit, mem_bb_hit, None
+    binary_bb_hit, libs_bb_hit = process_trace(logname, mem_map)
+
+    return binary_bb_hit, libs_bb_hit
 
 def process_trace(logname, maps):
     binary_bb_hit = list()
-    mem_bb_hit = dict()
+    libs_bb_hit = dict()
 
     for key, val in maps.iteritems():
-        mem_bb_hit[key] = list()
+        libs_bb_hit[key] = list()
 
     with open(logname, 'r') as log:
         lines = log.readlines()
-    # import IPython;IPython.embed()
+
     for line in lines:
-        # if 'end_code' in line:
-        #     end_code = line[line.find('0x'):].strip()
-        #     print end_code
-
-        # if 'start_code' in line:
-        #     start_code = line[line.find('0x'):].strip()
-        #     print start_code
-
         if 'Trace' in line:
             line = "0x" + line[line.find('[') + 1: line.rfind(']')].lstrip("0")
 
             if int(line, 16) >= int(start_code, 16) and int(line, 16) < int(end_code, 16):
                 binary_bb_hit.append(line)
             else:
-                # import IPython; IPython.embed()
                 for key, val in maps.iteritems():
                     try:
                         if int(line, 16) >= int(val[0], 16) and int(line, 16) < int(val[1], 16):
-                            mem_bb_hit[key].append(line)
+                            libs_bb_hit[key].append(line)
                             break
                     except:
                         pass
 
-    mem_bb_hit = calculate_lib_offset(maps, mem_bb_hit)
+    libs_bb_hit = calculate_lib_offset(maps, libs_bb_hit)
 
-    # with open(logname+'.trace', 'a+') as f:
-    #     for line in binary_bb_hit:
-    #         f.write(line)
-    #         f.write('\n')
+    testcase_name = logname[logname.rfind('/')+1:]
+    testcase_name = testcase_name[testcase_name.find('.')+1:testcase_name.rfind('.')]
 
-    return list(set(binary_bb_hit)), mem_bb_hit
+    filename = datetime.now().strftime(result_folder + logname[logname.rfind('/')+1:logname.rfind('.')]+"_hit_%H:%M:%S")
+    with open(filename, 'a+') as f:
+        for line in binary_bb_hit:
+            f.write(line)
+            f.write('\n')
+
+    for key, val in libs_bb_hit.iteritems():
+        filename = datetime.now().strftime(result_folder + key +'_'+testcase_name+'_%m-%d_%H:%M:%S')
+        with open(filename, 'a+') as f:
+            for line in val:
+                f.write(line)
+                f.write('\n')
+
+    return list(set(binary_bb_hit)), libs_bb_hit
 
 # if a "hit" addr not in bb addr lifted by ida, remove it
-def correct_ida_lift(hit_list, bb_list):
+def correct_static_analysis(hit_list, bb_list):
     for addr in hit_list:
         if addr not in bb_list:
             # hit_list.remove(addr)
@@ -157,16 +158,14 @@ def merge_hit(hit_list, so=False, name=None):
             return
         else:
             for addr in hit_list:
-                if addr not in so_global_hit[name]:
-                    so_global_hit[name].append(addr)
+                if addr not in so_overall_hit[name]:
+                    so_overall_hit[name].append(addr)
 
-        so_global_hit[name] = list(set(so_global_hit[name]))
+        so_overall_hit[name] = list(set(so_overall_hit[name]))
         return
     for addr in hit_list:
-        if addr not in global_hit:
-            global_hit.append(addr)
-
-        # global_hit = list(set(global_hit))
+        if addr not in binary_overall_hit:
+            binary_overall_hit.append(addr)
 
 def write_error_log(testcase, error_meg):
     error_folder = "error/"
@@ -178,7 +177,7 @@ def write_error_log(testcase, error_meg):
     f.write(error_meg + '\n')
     f.close()
 
-def capture_maps(pid):
+def get_mem_mapping(pid):
     res = dict()
     command = "pmap " + str(pid)
 
@@ -196,10 +195,8 @@ def capture_maps(pid):
         format_stdout.append(split_line)
 
     format_stdout = filter(None, format_stdout)
-    # import IPython; IPython.embed()
     for lib in lib_list:
         res[lib] = list()
-        # import IPython; IPython.embed()
         for line in format_stdout:
             try:
                 if 'x' in line[2] and lib == line[3]:
@@ -213,9 +210,6 @@ def capture_maps(pid):
                 pass
 
     for line in format_stdout:
-        # import IPython;IPython.embed()
-
-        # print program
         if 'x' in line[2] and line[3] in program:
             global start_code
             global end_code
@@ -238,19 +232,6 @@ def write_hit_log(hit_list):
 
     f.close()
 
-def find_start(target_binary):
-    command = 'readelf -h ' + target_binary
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout = proc.stdout.read()
-    stderr = proc.stdout.read()
-    if len(stderr) != 0:
-        return -1
-
-    stdout = stdout.split('\n')
-    for line in stdout:
-        if "Entry point address" in line:
-            return line[line.find('0x'):]
-
 def main():
     if 'QEMU' not in os.environ:
         print "Please set QEMU binary root. export QEMU=qemu_bin"
@@ -265,39 +246,34 @@ def main():
     global usermode_command
     usermode_command = os.environ['QEMU'] + '/qemu-x86_64 -d exec -D '
 
-    global lib_list
+
+    # Load configuration
+    global lib_list, program
+
     lib_list = config['library']
-
     file_input = config['file_input']
-    global program
     program = config['target_binary']
-    program_option = config['target_option']
-
-    if len(program_option) is not 0:
-        program_option = " ".join(program_option)
-    else:
-        program_option = ""
+    program_option = " ".join(config['target_option'])
     testcase = config['testcase']
-
-    if 'entry_addr' not in config or len(config['entry_addr']) == 0:
-        pause_addr = find_start(program)
-    else:
-        pause_addr = config['entry_addr']
-
     save_result = config['save_result']
-
     with_statistic = config['show_statistic']
-    # with_statistic =
-
     test_object = config['test_object']
-
     # draw_diagram = config.generate_report
 
-    if pause_addr == -1:
-        print "Please specified the entry address of the target binary"
-        sys.exit(2)
+    global result_folder
 
-    # all bb addrs lifted by ida
+    # Set up a log/result folder for certain binary
+    result_folder = program[program.rfind('/')+1:] + "_result/"
+    log_folder = program + "_trace/"
+
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
+
+    if not os.path.exists(result_folder):
+        os.makedirs(result_folder)
+
+
+    # read in static analysis result
     if with_statistic:
         addr_filename = program + '_addr'
         bb_list = capture_bb(addr_filename)
@@ -308,68 +284,72 @@ def main():
                 addr = f.readlines()
             lib_addr_dict[lib] = addr
 
-
+    # Initialize the so_overall_hit dictionary
     for item in lib_list:
-        so_global_hit[item] = list()
+        so_overall_hit[item] = list()
 
-    # Set up a log folder for certain binary
-    log_folder = program + "_trace/"
-
-    if not os.path.exists(log_folder):
-        os.makedirs(log_folder)
-
+    # Print out execution result
     print "Result"
 
+    # Print out static analysis result
     if with_statistic:
         print "Total Basic Blocks: " + str(len(bb_list))
         print "==========================================="
 
     for file in sorted(os.listdir(testcase)):
+        command = list()
+        command.append(program)
         if file_input:
-            command = program + ' ' + program_option + ' ' + os.path.join(testcase, file)
+            command.append(program_option)
+            command.append(os.path.join(testcase, file))
+
         else:
             with open(os.path.join(testcase, file), 'r') as testcase_file:
                 data = testcase_file.read().replace('\n', '')
-            command = program + ' ' + data
+            command.append(data)
 
         logname = log_folder +  program + '.' + file + '.log'
-        binary_bb_hit, mem_bb_hit, mem_bb = capture_log(command, logname, file,lib_list, pause_addr, with_statistic)
+
+        binary_bb_hit, libs_bb_hit = capture_log(command, logname, file,lib_list)
 
         """
         If QEMU execute a block is not lifted by ida, adjust bb_list
         """
+
         if with_statistic:
-            bb_list = correct_ida_lift(binary_bb_hit, bb_list)
+            bb_list = correct_static_analysis(binary_bb_hit, bb_list)
+            for key, val in libs_bb_hit.iteritems():
+                libs_bb_hit[key] = correct_static_analysis(val, lib_addr_dict[key])
 
+        # Update binary_overall_hit
         merge_hit(binary_bb_hit)
-
 
         print "Running " + file + ": "
         print "--Binary Hits:" + str(len(binary_bb_hit))
+
         if with_statistic:
             if test_object == 1 or test_object == 0:
                 coverage = statistic(binary_bb_hit, bb_list)
                 print "coverage: " + str(coverage) + '%'
-
-                print "bb in binary: " + str(len(binary_bb_hit))
-
+                print "bb hit in binary: " + str(len(binary_bb_hit))
 
         if test_object is 0 or test_object is 2:
-            # import IPython; IPython.embed()
             for lib in lib_list:
-                merge_hit(mem_bb_hit[lib], True, lib)
-                print "--"+ lib + " Hits:" + str(len(mem_bb_hit[lib]))
+                merge_hit(libs_bb_hit[lib], True, lib)
+                print "--"+ lib + " Hits:" + str(len(libs_bb_hit[lib]))
+
                 if with_statistic:
-                    so_coverage = statistic(mem_bb_hit[lib], mem_bb[lib])
+                    so_coverage = statistic(libs_bb_hit[lib], lib_addr_dict[lib])
                     print lib + " coverage: " + str(so_coverage) + '%'
             print ""
 
     print "==========================================="
+
     if with_statistic:
-        overall_coverage = statistic(global_hit, bb_list)
+        overall_coverage = statistic(binary_overall_hit, bb_list)
         print "Overall coverage: " + str(overall_coverage) + '%'
         print "Adjusted Total Basic Blocks: " + str(len(bb_list))
-    print "Binary Overall hit: " + str(len(global_hit))
+    print "Binary Overall hit: " + str(len(binary_overall_hit))
 
     if save_result:
         write_result(program)
@@ -402,23 +382,14 @@ def main():
     #         pg.generate_report(val, key)
 
 def write_result(program):
-    result_folder = program[program.rfind('/')+1:] + "_result/"
-
-    if not os.path.exists(result_folder):
-        os.makedirs(result_folder)
-
-    logname = result_folder + datetime.now().strftime(program[program.rfind('/')+1:] + '_coverage_%m-%d_%H:%M:%S')
+    logname = result_folder + datetime.now().strftime(program[program.rfind('/')+1:] + '_overall_%m-%d_%H:%M:%S')
     f = open(logname, "a+")
-    for addr in global_hit:
+    for addr in binary_overall_hit:
         f.write(addr)
         f.write('\n')
     f.close()
-    for key, val in so_global_hit.iteritems():
-        result_folder = key + "_result/"
-        if not os.path.exists(result_folder):
-            os.makedirs(result_folder)
-
-        logname = result_folder + datetime.now().strftime(key + "_coverage_%m-%d_%H:%M:%S")
+    for key, val in so_overall_hit.iteritems():
+        logname = result_folder + datetime.now().strftime(key + "_overall_%m-%d_%H:%M:%S")
         f = open(logname, "a+")
         for addr in val:
             f.write(addr)
