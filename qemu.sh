c4082c581acb69514efc00d9c99a5a1a1be89432
Jiakuan Li <jiakuan.li.cs@gmail.com>
2017-09-08 18:08:18 -0400

    edit qemu download script and new patch file

--- qemu.sh
+++ qemu.sh
@@ -2,9 +2,8 @@
 git clone https://github.com/qemu/qemu.git
 cd qemu
 git checkout v2.9.0
-patch -p1 < ../qemu_2.9.diff
+patch -p1 < ../qemu_patch.diff
 
 ./configure --target-list=x86_64-linux-user --enable-debug
 make -j4
 
-export QEMU=x86_64-linux-user
