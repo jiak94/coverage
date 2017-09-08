7745357253224aa1c88e4c98a731d004beb70418
Jiakuan Li <jiakuan.li.cs@gmail.com>
2017-09-08 18:08:45 -0400

    ida script to generate cfg

---
+++ generate.py
@@ -0,0 +1,33 @@
+import idaapi
+import idautils
+import idc
+import json
+def analysis():
+    all_funcs = idautils.Functions()
+    overall_addr = dict()
+
+    for f in all_funcs:
+        f = idaapi.FlowChart(idaapi.get_func(f),flags=idaapi.FC_PREDS)
+        for block in f:
+            key = ''
+            # overall_addr.append(hex(block.startEA))
+            key += hex(block.startEA)
+
+            key += ','
+            key += hex(idc.PrevHead(block.endEA))
+            sus_addr = list()
+            successor = block.succs()
+            for addr in successor:
+                sus_addr.append(hex(addr.startEA))
+
+            overall_addr[key] = sus_addr
+
+    filename = idc.GetInputFile() + "_cfg"
+    with open(filename, 'w') as f:
+        json.dump(overall_addr, f)
+
+def main():
+    analysis()
+    print "Finished!"
+if __name__ == "__main__":
+    main()
